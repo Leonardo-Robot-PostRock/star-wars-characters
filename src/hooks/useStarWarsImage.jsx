c050f33b96231bad6156fb2868986decc5e13398
Leonardo Puebla <leonardo38361@gmail.com>
2022-04-10 21:02:31 -0300

    useStarWarsImage, mostrar imagenes con una transición de 3s

--- src/hooks/useStarWarsImage.jsx
+++ src/hooks/useStarWarsImage.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useRef, useState } from "react";
 
-const useStarWarsImage = () => {
-    const images = [
-        
-    ]
-    
-    return (
-        
-    );
-}
+const useStarWarsImage = (images) => {
+  const slidePresentationTime = 3000;
+  const [currentSlide, setCurrentSlide] = useState(0);
+  let sliderInterval = useRef();
 
+  useEffect(() => {
+    sliderInterval = setInterval(() => {
+      setCurrentSlide((currentSlide + 1) % images.length);
+    }, slidePresentationTime);
+    return () => {
+      clearInterval(sliderInterval);
+    };
+  });
+  return currentSlide;
+};
 export default useStarWarsImage;
